13b10d9c4eb1117da4f625e445b498b73411ce38
berose313 <berose995@gmail.com>
2022-07-07 15:19:54 +0400

    added basic routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Registration from "./pages/registration/Registration";
+import Home from "./pages/home/Home";
+import Experience from "./pages/experience/Experience";
+import Last from "./pages/last/Last";
 
 function App() {
-   return <div className="App"></div>;
+   return (
+      <div className="App">
+         <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/registration" element={<Registration />} />
+            <Route path="/experience" element={<Experience />} />
+            <Route path="/last" element={<Last />} />
+         </Routes>
+      </div>
+   );
 }
 
 export default App;
---
+++ src/pages/last/Last.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Last.css";
+
+const Last = () => {
+   return <div>Last</div>;
+};
+
+export default Last;
