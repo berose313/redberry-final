8582b127eab8a8b198fd61aa5a24df5dc88d9a7a
berose313 <berose995@gmail.com>
2022-07-07 17:06:19 +0400

    created last page

--- src/pages/last/Last.jsx
+++ src/pages/last/Last.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "./Last.css";
 
+import Background from "../../UI/Background";
+import LastImage from "../../Images/last.png";
+import LastLogo from "../../Images/last-logo.png";
+
 const Last = () => {
-   return <div>Last</div>;
+   return (
+      <div>
+         <Background image={LastImage} />
+         <div className="last">
+            <img src={LastLogo} alt="" className="last-logo" />
+         </div>
+      </div>
+   );
 };
 
 export default Last;
