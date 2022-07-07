3aa3d7b588860325358792bf060d146eb9c19c31
berose313 <berose995@gmail.com>
2022-07-07 16:00:34 +0400

    created home page and added routing to its button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 
 import Registration from "./pages/registration/Registration";
 import Home from "./pages/home/Home";
@@ -7,10 +7,21 @@ import Experience from "./pages/experience/Experience";
 import Last from "./pages/last/Last";
 
 function App() {
+   const navigator = useNavigate();
+
    return (
       <div className="App">
          <Routes>
-            <Route path="/home" element={<Home />} />
+            <Route
+               path="/home"
+               element={
+                  <Home
+                     next={() => {
+                        navigator("/registration");
+                     }}
+                  />
+               }
+            />
             <Route path="/registration" element={<Registration />} />
             <Route path="/experience" element={<Experience />} />
             <Route path="/last" element={<Last />} />
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import "./Home.css";
-import Background from "../../UI/Background";
 
-const Home = () => {
+import Background from "../../UI/Background";
+import homeImage from "../../Images/home.png";
+
+const Home = (props) => {
    return (
       <div>
-         <Background />
+         <Background image={homeImage} />
+         <div className="home">
+            <div>
+               <h1 className="home-heading">
+                  chess says <span>a lot about</span>
+               </h1>
+               <h1 className="home-heading">who we are</h1>
+            </div>
+            <button className="home-button" onClick={props.next}>
+               get started
+            </button>
+         </div>
       </div>
    );
 };
