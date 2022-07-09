7ff6be5ae089c19aacce3f4b30ad97d74ceb5fca
berose313 <berose995@gmail.com>
2022-07-09 11:48:42 +0400

    built custom select list and added basic functionality to it

--- src/pages/experience/Experience.jsx
+++ src/pages/experience/Experience.jsx
@@ -1,8 +1,65 @@
 import React from "react";
+import Background from "../../UI/Background";
 import "./Experience.css";
+import ExperienceImage from "../../Images/experience.png";
+import { useState } from "react";
+
+const levels = [
+   { level: "Beginner" },
+   { level: "Intermediate" },
+   { level: "Professional" },
+];
+
+const Options = (props) => {
+   return (
+      <div className="options">
+         {levels.map((el) => {
+            return (
+               <div
+                  className=""
+                  onClick={() => {
+                     props.placeholder(el.level);
+                     props.disableOptions();
+                  }}
+               >
+                  {el.level}
+               </div>
+            );
+         })}
+      </div>
+   );
+};
 
 const Experience = () => {
-   return <div>Experience</div>;
+   const [options, setOptions] = useState();
+   const [placeholder, setPlaceholder] = useState("level of knowledge *");
+
+   const currentEl = (element) => {
+      setPlaceholder(element);
+   };
+
+   const disableOptions = () => {
+      setOptions();
+   };
+
+   return (
+      <div>
+         <div
+            className="level"
+            onClick={() => {
+               setOptions(
+                  <Options
+                     placeholder={currentEl}
+                     disableOptions={disableOptions}
+                  />
+               );
+            }}
+         >
+            {placeholder}
+         </div>
+         {options}
+      </div>
+   );
 };
 
 export default Experience;
