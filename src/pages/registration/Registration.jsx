48bf864ffe06d5e64c745a1af6cd0b8641871330
berose313 <berose995@gmail.com>
2022-07-09 14:57:35 +0400

    added basic validation, also it preserves inputs after reloading

--- src/pages/registration/Registration.jsx
+++ src/pages/registration/Registration.jsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 import "./Registration.css";
 
 import Background from "../../UI/Background";
 import RegistrationImage from "../../Images/registration.png";
 
 const Registration = (props) => {
+   const [name, setName] = useState(localStorage.getItem("name"));
+   const [email, setEmail] = useState(localStorage.getItem("email"));
+   const [number, setNumber] = useState(localStorage.getItem("number"));
+   const [date, setDate] = useState(localStorage.getItem("date"));
+
+   let data = [];
+
+   const submitHandler = (event) => {
+      event.preventDefault();
+      data.push({
+         name,
+         email,
+         number,
+         date,
+      });
+
+      localStorage.setItem("name", name);
+      localStorage.setItem("email", email);
+      localStorage.setItem("number", number);
+      localStorage.setItem("date", date);
+
+      // props.takeData(data);
+      props.next();
+   };
+
    return (
-      <div>
+      <Fragment>
          <Background image={RegistrationImage} />
          <div className="reg-headings">
             <div className="reg-header">Start creating your account</div>
@@ -24,30 +49,88 @@ const Registration = (props) => {
                <h1>Personal information</h1>
                <h3>This is basic informaton fields</h3>
             </div>
-            <form action="" className="reg-form">
+            <form action="" className="reg-form" onSubmit={submitHandler}>
                <input
                   type="text"
                   placeholder="Name"
                   required
                   className="reg-form-input"
+                  value={localStorage.getItem("name")}
+                  name="name"
+                  onChange={(event) => {
+                     setName(event.target.value);
+
+                     if (event.target.value.length < 3) {
+                        setTimeout(() => {
+                           event.target.style.backgroundColor = "#FFEFEF";
+                        }, 1000);
+                     } else {
+                        setTimeout(() => {
+                           event.target.style.backgroundColor = "#3BAF77";
+                        }, 1000);
+                     }
+                  }}
                />
                <input
                   type="email"
                   placeholder="Email address"
                   required
                   className="reg-form-input"
+                  value={localStorage.getItem("email")}
+                  name="email"
+                  onChange={(event) => {
+                     setEmail(event.target.value);
+
+                     if (!event.target.value.includes("@redberry.ge")) {
+                        setTimeout(() => {
+                           event.target.style.backgroundColor = "#FFEFEF";
+                        }, 200);
+                     } else {
+                        setTimeout(() => {
+                           event.target.style.backgroundColor = "#3BAF77";
+                        }, 200);
+                     }
+                  }}
                />
                <input
                   type="number"
                   placeholder="Phone number"
                   required
                   className="reg-form-input"
+                  value={localStorage.getItem("number")}
+                  name="number"
+                  onChange={(event) => {
+                     setNumber(event.target.value);
+
+                     if (event.target.value.length < 8) {
+                        setTimeout(() => {
+                           event.target.style.backgroundColor = "#FFEFEF";
+                        }, 200);
+                     } else {
+                        setTimeout(() => {
+                           event.target.style.backgroundColor = "#3BAF77";
+                        }, 200);
+                     }
+                  }}
                />
                <input
                   type="date"
                   placeholder="Date of birth"
                   required
                   className="reg-form-input"
+                  value={localStorage.getItem("date")}
+                  name="date"
+                  onChange={(event) => {
+                     setDate(event.target.value);
+
+                     if (event.target.value.length < 2) {
+                        setTimeout(() => {
+                           event.target.style.backgroundColor = "#FFEFEF";
+                        }, 200);
+                     } else {
+                        event.target.style.backgroundColor = "#3BAF77";
+                     }
+                  }}
                />
                <div className="reg-form-btns">
                   <button className="reg-btn-white" onClick={props.prev}>
@@ -56,14 +139,14 @@ const Registration = (props) => {
                   <button
                      className="reg-btn-black"
                      type="submit"
-                     onClick={props.next}
+                     // onClick={props.next}
                   >
                      next
                   </button>
                </div>
             </form>
          </div>
-      </div>
+      </Fragment>
    );
 };
 
